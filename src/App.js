8895552430dfbe5263f365db1f6ee2061f1bea66
isaacw2as <isaacyun03@gmail.com>
2022-10-23 00:07:05 -0700

    committing for carl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
-
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { Home, About, Explore } from './pages';
 
 const App = () => {
   return (
-    <div>
-      <h1>DEEZ</h1>
+    <Router>
+    <div className='App'>
+      <div className='content'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/explore' element={<Explore />}></Route>
+        </Routes>
+      </div>
     </div>
+    </Router>
   )
 }
 
